22f9861442acb52abded274ed5f121085ded1678
Firdaus Razak <razakfirdaus95@gmail.com>
2022-07-25 16:20:40 +0800

    first com

---
+++ README.md
@@ -0,0 +1,1 @@
+#ssfworkshop14
---
+++ app/src/test/java/visa/ssfworkshop14/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package visa.ssfworkshop14.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
